e423f00bdf23270aaed35a5ef7cb07223e2a4138
Printan <printan.nun@dome.tu.ac.th>
2025-10-06 20:44:10 +0700

    Add Reservation.java to create table

---
+++ src/main/java/com/example/tu_bookingsports/model/Reservations.java
@@ -0,0 +1,136 @@
+package com.example.demo.model;
+import jakarta.persistence.*;
+import java.util.UUID;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+@Entity
+@Table(name = "reservations")
+public class Reservations {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(name = "reservation_id" , updatable = false , nullable = false)
+    private UUID reservationID;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @Column(name = "room_id", nullable = false)
+//    private Room room;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id", nullable = false)
+//    private User user;
+
+    @Column(name = "start_time", nullable = false)
+    private LocalTime startTime;
+
+    @Column(name = "end_time", nullable = false)
+    private LocalTime endTime;
+
+    public enum ReservationStatus {
+        PENDING,     // 0 รอการยืนยัน
+        CONFIRMED,   // 1 ยืนยันแล้ว
+        CANCELLED,   // 2 ยกเลิก
+    }
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private ReservationStatus status;
+
+    @Column(name = "created", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated", nullable = false)
+    private LocalDateTime updatedAt;
+
+    //Constuctor
+    public Reservations() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+        this.status = ReservationStatus.PENDING;
+    }
+
+    public Reservations(LocalTime startTime, LocalTime endTime, ReservationStatus status) { //Room room, User user
+//        this.room = room;
+//        this.user = user;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.status = status != null ? status : ReservationStatus.PENDING;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    //Setter Getter
+    public UUID getReservationID() {
+        return reservationID;
+    }
+
+    public void setReservationID(UUID reservationID) {
+        this.reservationID = reservationID;
+    }
+
+//    public Room getRoom() {
+//        return room;
+//    }
+//
+//    public void setRoom(Room room) {
+//        this.room = room;
+//    }
+//
+//    public User getUser() {
+//        return user;
+//    }
+//
+//    public void setUser(User user) {
+//        this.user = user;
+//    }
+
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public ReservationStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ReservationStatus status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+}
