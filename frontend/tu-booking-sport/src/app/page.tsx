6fdd5ea0cd12e8d280ab06de87d4c851367d37f9
Phatsakon Sonsang <phatsakon.son@dome.tu.ac.th>
2025-10-09 00:12:19 +0700

    Change global_fond and add Forgot_page

--- frontend/tu-booking-sport/src/app/page.tsx
+++ frontend/tu-booking-sport/src/app/page.tsx
@@ -1,15 +1,15 @@
 export default function Home() {
   return (
-    <div className="bg-white-100 flex items-center justify-center min-h-screen">
-      <div className="relative border-2 border-black rounded-[10px] px-[20px] pt-[40px] pb-[20px] w-[300px] bg-white">
-        <h2 style={{ fontFamily: "'Spectral', serif" }}
-        className="absolute -top-[15px] left-[100px] -translate-x-1/2 bg-gray-50 px-[10px] font-bold text-2xl">
+    <div className="bg-gray-50 flex items-center justify-center min-h-screen">
+      <div className="relative border-2 border-black rounded-[10px] px-[20px] pt-[40px] pb-[20px] w-[300px] bg-gray-50">
+        <h2 className="absolute -top-[15px] left-[100px] -translate-x-1/2 bg-gray-50 px-[10px] font-bold text-2xl">
         LOG-IN
         </h2>
 
         <form className="space-y-5">
-        <div>
-          <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-1">E-mail / ID*</label>
+        <div className="relative">
+          <label htmlFor="email" className="absolute -top-[10px] left-[10px] block text-sm font-medium text-gray-700 mb-1 bg-gray-50">
+            E-mail / ID*</label>
             <div className="relative">
               <input type="email" id="email" placeholder="your@dome.tu.ac.th"
                 className="w-full px-4 py-2 pl-5 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black" />
@@ -17,11 +17,11 @@ export default function Home() {
             </div>
         </div>
 
-        <div>
-          <label htmlFor="password" className="block text-sm font-medium text-gray-700 mb-1">Password*</label>
+        <div className="relative">
+          <label htmlFor="password" className="absolute -top-[10px] left-[10px] block text-sm font-medium text-gray-700 mb-1 bg-gray-50">Password*</label>
             <div className="relative">
-              <input type="password" id="password" placeholder="••••••••"
-              className="w-full px-4 py-2 pl-5 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black" />
+              <input type="password" id="password" placeholder="Password"
+              className="w-full px-4 py-2 pl-5 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black"/>
               <span className="absolute right-3 top-2.5 text-gray-400">🔑</span>
             </div>
         </div>
