2a4ca063066761e34900c1010ed4752232e1eca6
Nattanan Apirakcheewan <nuttanan.api@dome.tu.ac.th>
2025-10-22 02:04:39 +0700

    refine commit HomepageController.java

---
+++ backend/TU_BookingSports/src/main/java/com/example/tu_bookingsports/controller/HomepageController.java
@@ -0,0 +1,26 @@
+package com.example.tu_bookingsports.controller;
+
+import com.example.tu_bookingsports.DTO.HomepageResponse;
+import com.example.tu_bookingsports.service.HomepageService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+
+@CrossOrigin(origins = "http://localhost:3000")
+
+@RestController
+@RequestMapping("/homepage")
+public class HomepageController {
+
+    private final HomepageService homepageService;
+
+    public HomepageController(HomepageService homepageService) {
+        this.homepageService = homepageService;
+    }
+
+    @GetMapping
+    public List<HomepageResponse> getHomepage() {
+        return homepageService.getHomepageData();
+    }
+}
